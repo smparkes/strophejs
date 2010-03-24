a678aad2e310628996c4eec446d59103d7bd4610
Steven Parkes <smparkes@smparkes.net>
2010-03-24 13:50:52 -0700

    More verbose callbacks on connfail
    * Use stream:error contents when available on connection failure
    * Add emacs mode line
    * Print stacktraces on error as available

--- src/core.js
+++ src/core.js
@@ -1,3 +1,4 @@
+/* -*- tab-width: 4; c-basic-offset: 4; espresso-indent-level:4 -*- */
 /*
     This program is distributed under the terms of the MIT license.
     Please see the LICENSE file for details.
@@ -2012,8 +2013,12 @@ Strophe.Connection.prototype = {
             try {
                 this.connect_callback(status, condition);
             } catch (e) {
-                Strophe.error("User connection callback caused an " +
-                              "exception: " + e);
+                var msg = "User connection callback caused an " +
+                           "exception: " + e;
+                if (e.stack) {
+                    msg = msg + "\n" + e.stack;
+                }
+                Strophe.error(msg);
             }
         }
     },
@@ -2500,10 +2505,18 @@ Strophe.Connection.prototype = {
         if (typ !== null && typ == "terminate") {
             // an error occurred
             cond = bodyWrap.getAttribute("condition");
-            conflict = bodyWrap.getElementsByTagName("conflict");
             if (cond !== null) {
-                if (cond == "remote-stream-error" && conflict.length > 0) {
-                    cond = "conflict";
+                if (cond == "remote-stream-error") {
+                    conflict = bodyWrap.getElementsByTagName("conflict");
+                    if (conflict.length>0) {
+                        cond = "conflict";
+                    } else {
+                        conflict =
+                            bodyWrap.getElementsByTagNameNS("http://etherx.jabber.org/streams","error");
+                        if (conflict.length>0) {
+                            cond = conflict[0].children[0].nodeName;
+                        }
+                    }
                 }
                 this._changeConnectStatus(Strophe.Status.CONNFAIL, cond);
             } else {
